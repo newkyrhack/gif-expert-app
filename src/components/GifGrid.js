1a1ae591bb7887593efa58c73c0eb6e7bc42ca96
cesar serrano <kayser.fb@hotmail.com>
2021-04-25 19:36:18 -0500

    Initialize project using Create React App

---
+++ src/components/GifGrid.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useFetchGifs } from '../hooks/useFetchGifs'
+import { GifGridItem } from './GifGridItem';
+
+export const GifGrid = ({category}) => {
+    const {loading, data:images} = useFetchGifs(category);
+    return (
+        <>
+        <h3 className='card animate__animated animate__fadeIn'>{category}</h3>
+        {loading&&<p className='card animate__animated animate__flash'>'Cargando'</p>}
+        <div className="card-grid">
+            {images.map( img => <GifGridItem key={img.id} img={img}/>)}
+        </div>
+        </>
+    )
+}
---
+++ src/helpers/getGifs.js
@@ -0,0 +1,12 @@
+export const getGifs = async (category) => {
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=JSyZarB3mxVQeSnML1QfMu4vdIKdp4OM`;
+    const {data} = await ( await fetch(url)).json();
+    const gifs = data.map(img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images.downsized_medium.url
+        }
+    });
+    return gifs;
+}
